036c47e27a2253e0cf7757e45c718e187eb6c47d
nguyenphuhuy0405 <nguyenphuhuybgg@gmail.com>
2023-07-09 16:26:32 +0700

    Create routes, app/controllers

--- src/index.js
+++ src/index.js
@@ -5,9 +5,17 @@ const handlebars = require('express-handlebars').engine
 const app = express()
 const port = 3000
 
+const route = require('./routes')
+
 app.use(express.static(path.join(__dirname,'/public')))
+
+app.use(express.urlencoded({
+  extended: true,
+}))
+app.use(express.json())
+
 // HTTP logger
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 
 //Template engine
 app.engine('hbs', handlebars({
@@ -16,14 +24,8 @@ app.engine('hbs', handlebars({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources\\views'));
 
-
-app.get('/', (req, res) => {
-  res.render('home')
-})
-
-app.get('/news', (req, res) => {
-  res.render('news')
-})
+//Routes init
+route(app)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
